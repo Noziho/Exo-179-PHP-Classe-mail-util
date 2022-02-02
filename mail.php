6c677ceef66bb8607c9eecba8d1328c0b36d911c
Noah2211 <noah.decroix3@gmail.com>
2022-02-02 11:58:47 +0100

    add class mail

---
+++ mail.php
@@ -0,0 +1,58 @@
+<?php
+class mail {
+   public $sujet;
+   public $corpsMessage;
+   public $destinataire;
+
+    /**
+     * @return mixed
+     */
+    public function getSujet()
+    {
+        return $this->sujet;
+    }
+
+    /**
+     * @param mixed $sujet
+     */
+    public function setSujet($sujet): void
+    {
+        $this->sujet = $sujet;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCorpsMessage()
+    {
+        return $this->corpsMessage;
+    }
+
+    /**
+     * @param mixed $corpsMessage
+     */
+    public function setCorpsMessage($corpsMessage): void
+    {
+        $this->corpsMessage = $corpsMessage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestinataire()
+    {
+        return $this->destinataire;
+    }
+
+    /**
+     * @param mixed $destinataire
+     */
+    public function setDestinataire($destinataire): void
+    {
+        $this->destinataire = $destinataire;
+    }
+
+    public function sendMail () :void {
+        mail();
+    }
+}
